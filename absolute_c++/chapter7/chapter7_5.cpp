88290923bfee8edc146954488abe7379ddf7b5c8
edeniz2021 <edeniz2021@gtu.edu.tr>
2023-03-22 15:00:17 +0300

    add new solution 7.5

--- absolute_c++/chapter7/chapter7_5.cpp
+++ absolute_c++/chapter7/chapter7_5.cpp
@@ -1,3 +1,33 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+
+int main()
+{
+    int n; // number of suitors
+    cout << "Enter the number of suitors: ";
+    cin >> n;
+    vector<int> suitor(n);
+    int count = 0;
+    cout << "İnitial List: ";
+    for(int i=0;i<n;i++)
+    {
+        suitor[i] = i+1;
+        cout <<  suitor[i] ;
+    }
+    cout << endl;
+    while(suitor.size()>1)
+    {
+         for(int i = 0; i<suitor.size(); i++){
+            count += 1;
+            if(count == 3){// eliminate the i-th suitor
+                cout << "The " << suitor[i] << " suitor is eliminated." << endl;
+                suitor.erase(suitor.begin() + i);// reset the counter and the index
+                count = 0;
+                i--;
+            }
+        }
+    }
+    cout << "The winning suitor is " << suitor[0] << endl;
+}
