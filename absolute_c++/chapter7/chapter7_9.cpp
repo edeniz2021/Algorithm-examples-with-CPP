ead70d2adbff38df94b89d5bc7c76094290edff8
edeniz2021 <edeniz2021@gtu.edu.tr>
2023-03-23 12:15:29 +0300

    add new exercise

---
+++ absolute_c++/chapter7/chapter7_9.cpp
@@ -0,0 +1,54 @@
+/*Prior to 2009 the bar code on an envelope used by the U.S. Postal Service repre- sented a five (or more) digit zip code using a format called POSTNET. The bar code consists of long and short bars as shown here:
+For this program, we will represent the bar code as a string of digits. The digit 1 represents a long bar, and the digit 0 represents a short bar. Therefore, the bar code shown would be represented in our program as follows:
+   110100101000101011000010011
+The first and last digits of the bar code are always 1. Removing these leave 25 digits. If these 25 digits are split into groups of five digits each then we have the following:
+   10100 10100 01010 11000 01001
+Next, consider each group of five digits. There always will be exactly two 1’s in each group of digits. Each digit stands for a number. From left to right, the digits encode the values 7, 4, 2, 1, and 0. Multiply the corresponding value with the digit and compute the sum to get the final encoded digit for the zip code. The following table shows the encoding for 10100.
+Zip Code Digit = 7 + 0 + 2 + 0 + 0 = 9
+Repeat this for each group of five digits and concatenate to get the complete zip code. There is one special value. If the sum of a group of five digits is 11, then this represents the digit 0 (this is necessary because with two digits per group it is not possible to represent zero). The zip code for the sample bar code decodes to 99504. While the POSTNET scheme may seem unnecessarily complex, its design allows machines to detect whether errors have been made in scanning the zip code.
+Bar Code Digits
+1 0 1 0 0
+Value
+7 4 2 1 0
+Product of Digit * Value
+7 0 2 0 0
+Write a zip code class that encodes and decodes five-digit bar codes used by the U.S. Postal Service on envelopes. The class should have two constructors. The first constructor should input the zip code as an integer, and the second constructor should input the zip code as a bar code string consisting of 0’s and 1’s as described above. Although you have two ways to input the zip code, internally, the class should only store the zip code using one format. (You may choose to store it as a bar code string or as a zip code number.) The class also should have at least two public member functions: one to return the zip code as an integer and the other to return the zip code in bar code format as a string. All helper functions should be declared private. Embed your class definition in a suitable test program.*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Posta
+{
+public:
+    Posta();
+    void calBarCod(string kod);
+    void calCodBar();
+
+private:
+    string barCod;
+    int value[5];
+};
+Posta::Posta()
+{
+    barCod = "";
+    value[5] = {7, 4, 2, 1, 0};
+}
+
+void Posta::calBarCod(string kod)
+{
+    for (int i = 0; i < 27; i++)
+    {
+        for (int j = 0; j < 25; j++)
+        {
+            if(i!=0 || i!=26)
+            {
+            barCod[j]= kod[i];
+            }
+        }
+    }
+}
+int main()
+{
+}
