2187951b1a10a9be202fe71b7699e3e90c52a87c
edeniz2021 <edeniz2021@gtu.edu.tr>
2023-03-27 12:45:29 +0300

    add new exercise

---
+++ absolute_c++/chapter8/chapter8_3.cpp
@@ -0,0 +1,44 @@
+/*Define a class for complex numbers. A complex number is a number of the form
+a + b*i
+ where for our purposes, a and b are numbers of type double , and i is a number
+that represents the quantity root(-1) . Represent a complex number as two values of type double . Name the member variables real and imaginary . (The variable
+for the number that is multiplied by i is the one called imaginary .) Call
+the class Complex . Include a constructor with two parameters of type double
+that can be used to set the member variables of an object to any values. Include a
+constructor that has only a single parameter of type double ; call this parameter
+realPart and define the constructor so that the object will be initialized to
+realPart + 0*i . Include a default constructor that initializes an object to 0 (that is, to
+0 + 0*i ). Overload all the following operators so that they correctly apply to the
+type Complex : == , + , - , * , >> , and << . You should also write a test program to test
+your class. Hints: To add or subtract two complex numbers, add or subtract the
+two member variables of type double . The product of two complex numbers is
+given by the following formula:
+(a + b*i)*(c + d*i) = = (a*c - b*d) + (a*d + b*c)*i
+In the interface file, you should define a constant i as follows:
+const Complex i(0, 1);
+This defined constant i will be the same as the i discussed above. */
+
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+class Complex
+{
+public:
+    Complex();
+    Complex(double r, double i);
+    Complex operator+(const Complex &C);
+    Complex operator-(const Complex &C);
+    Complex operator*(const Complex &C);
+    bool operator==(const Complex &C);
+    double getReal() const { return real; }
+    double getImagine() const { return imaginary; }
+    friend ostream &operator<<(ostream &outputStream, const Complex &C);
+    friend istream &operator<<(istream &inputStream, Complex &C);
+
+private:
+    double real;
+    double imaginary;
+};
+const Complex i(0, 1);
