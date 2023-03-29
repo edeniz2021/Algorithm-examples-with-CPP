2b99f8be382a4eba333797ef257371eb70545ffc
edeniz2021 <edeniz2021@gtu.edu.tr>
2023-03-29 11:02:14 +0300

    add completed code 8.7

--- absolute_c++/chapter8/chapter8_7.cpp
+++ absolute_c++/chapter8/chapter8_7.cpp
@@ -1,31 +1,95 @@
-/*Define a class named PrimeNumber that stores a prime number. 
-The default con- structor should set the prime number to 1. Add another constructor that allows the caller to set the prime number. 
-Also, add a function to get the prime number. Finally, overload the prefix and postfix ++ and −− operators so they return a Prime- Number object that is the next largest prime number (for ++) and the next smallest prime number (for −−). 
+/*Define a class named PrimeNumber that stores a prime number.
+The default con- structor should set the prime number to 1. Add another constructor that allows the caller to set the prime number.
+Also, add a function to get the prime number. Finally, overload the prefix and postfix ++ and −− operators so they return a Prime- Number object that is the next largest prime number (for ++) and the next smallest prime number (for −−).
 For example, if the object’s prime number is set to 13, then invoking ++ should return a PrimeNumber object whose prime number is set to 17. Create an appropriate test program for the class.*/
 #include <iostream>
+
 using namespace std;
 
 class PrimeNumber
 {
-    public:
-    PrimeNumber();
-    PrimeNumber(int num);
-    void setPrime(int num);
-    int getPrime()const{ return prime; }
-    private:
+public:
+    PrimeNumber() : prime(1) {}
+    PrimeNumber(int n) : prime(n) {}
+    int get() const { return prime; }
+    PrimeNumber &operator++();
+    PrimeNumber operator++(int);
+    PrimeNumber &operator--();
+    PrimeNumber operator--(int);
+
+private:
     int prime;
+    bool isPrime(int n);
+    int nextPrime;
 };
-PrimeNumber::PrimeNumber()
+
+bool PrimeNumber::isPrime(int n)//check the prime number
 {
-    prime = 0;
+    if (n < 2)
+        return false;
+
+    for (int i = 2; i <n/2; ++i)
+        if (n % i == 0)
+            return false;
+
+    return true;
 }
-PrimeNumber::PrimeNumber(int num)
+PrimeNumber& PrimeNumber::operator++()//postfix increment
 {
-    prime = num;
-}
-void PrimeNumber::setPrime(int num)
-{
-    prime = num;
+    prime++;
+    while (!isPrime(prime)) {
+        prime++;
+    }
+    return (*this);
 }
 
+PrimeNumber PrimeNumber::operator++(int)//prefix increment
+{
+    PrimeNumber temp(*this);
+    prime++;
+    while (!isPrime(prime)) {
+        prime++;
+    }
+    return temp;
+}
 
+PrimeNumber& PrimeNumber::operator--()//postfix decrement
+{
+    prime--;
+    while (!isPrime(prime)) {
+        prime--;
+    }
+    return (*this);
+}
+
+PrimeNumber PrimeNumber::operator--(int)//postfix increment
+{
+    PrimeNumber temp(*this);
+    prime--;
+    while (!isPrime(prime)) {
+        prime--;
+    }
+    return temp;
+}
+
+int main()
+{
+    PrimeNumber p1, p2(13);
+
+    cout << "p1 = " << p1.get() << endl;
+    cout << "p2 = " << p2.get() << endl;
+
+    ++p2;
+    cout << "p2 = " << p2.get() << endl;
+
+    p2++;
+    cout << "p2 = " << p2.get() << endl;
+
+    --p2;
+    cout << "p2 = " << p2.get() << endl;
+
+    p2--;
+    cout << "p2 = " << p2.get() << endl;
+
+    return 0;
+}
