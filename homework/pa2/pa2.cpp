51a8fc318e8d8a0a2e7774014f655c8400827da3
edeniz2021 <edeniz2021@gtu.edu.tr>
2023-03-30 08:01:56 +0300

    add new code

--- homework/pa2/pa2.cpp
+++ homework/pa2/pa2.cpp
@@ -13,10 +13,14 @@ public:
     void openImage();
     void saveImage();
     void scriptImage();
+    int getWidth() const { return width; }
+    int getHeigth() const { return heigth; }
+    int getPix() const { return pix; }
 
 private:
-    string filename;
-    int width, heigth;
+    string filename, format;
+    int width, heigth, pix;
+    vector<vector<int> > pixel;
 };
 Image::Image()
 {
@@ -25,8 +29,7 @@ Image::Image()
 void Image::Menu()
 {
     int button = 1;
-    int buttonTwo = 1;
-    int choise;
+    int buttonTwo;
     while (button != 0)
     {
         cout << "MAIN MENU" << endl;
@@ -37,6 +40,7 @@ void Image::Menu()
         cin >> button;
         if (button == 1)
         {
+            buttonTwo = 1;
             while (buttonTwo != 0)
             {
                 cout << "OPEN AN IMAGE MENU" << endl;
@@ -51,11 +55,13 @@ void Image::Menu()
         }
         else if (button == 2)
         {
+            buttonTwo = 1;
             while (buttonTwo != 0)
             {
                 cout << "SAVE IMAGE DATA MENU" << endl;
                 cout << "0 - UP" << endl;
                 cout << "1 - Enter A File Name" << endl;
+                cin >> buttonTwo;
                 if (buttonTwo == 1)
                 {
                     saveImage();
@@ -64,11 +70,13 @@ void Image::Menu()
         }
         else if (button == 3)
         {
+            buttonTwo = 1;
             while (buttonTwo != 0)
             {
                 cout << "CONVERT TO GRAYSCALE MENU" << endl;
                 cout << "0 - UP" << endl;
                 cout << "1 - Enter Coefficients For RED GREEN And BLUE Channels." << endl;
+                cin >> buttonTwo;
                 if (buttonTwo == 1)
                 {
                     scriptImage();
@@ -76,22 +84,68 @@ void Image::Menu()
             }
         }
         else
+        {
             exit(0);
+        }
     }
 }
 void Image::openImage()
 {
+    const int pixelDigit = 3;
     string inputName;
-    cin>> inputName;
+    cin >> inputName;
     ifstream file(inputName);
-    if(file.is_open())
+    if (file.is_open())
     {
-
+        getline(file, format);
+        file >> width >> heigth >> pix;
+        for (int i = 0; i < heigth; i++)
+        {
+            pixel.push_back(vector<int>());
+            for (int j = 0; j < width * pixelDigit; j++)
+            {
+                pixel[i].push_back(0);
+            }
+        }
+        for (int i = 0; i < heigth; i++)
+        {
+            for (int j = 0; j < width * pixelDigit; j++)
+            {
+                int x;
+                if (file.good())
+                {
+                    file >> x;
+                    pixel[i][j] = x;
+                }
+            }
+        }
+        file.close();
     }
-
+    else cout << "File is not read!" << endl;
 }
+
 void Image::saveImage()
 {
+    string inputfile;
+    cin >> inputfile;
+    ofstream file(inputfile);
+    if (file.is_open())
+    {
+        file << format << endl;
+        file << getWidth() << " " << getHeigth() << endl;
+        file << getPix() << endl;
+        for (int i = 0; i < getWidth(); i++)
+        {
+            for (int j = 0; j < getHeigth() * 3; j++)
+            {
+                file << pixel[i][j] << " ";
+            }
+            file << endl;
+        }
+        file.close();
+    }
+    else
+        cout << "Not write txt!" << endl;
 }
 void Image::scriptImage()
 {
