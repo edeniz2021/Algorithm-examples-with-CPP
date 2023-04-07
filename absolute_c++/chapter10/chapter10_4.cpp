b01649264b1ec7b068a7cbca6749c1430fa939d3
edeniz2021 <edeniz2021@gtu.edu.tr>
2023-04-07 23:16:48 +0300

    add new solution 10.4

--- absolute_c++/chapter10/chapter10_4.cpp
+++ absolute_c++/chapter10/chapter10_4.cpp
@@ -14,3 +14,72 @@ empty list.
 • An overloaded assignment operator that correctly makes a new copy of the list of courses.
 • A destructor that releases all memory that has been allocated.
 Write a main function that tests all of your functions.*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+class Student
+{
+    public:
+    Student();
+    ~Student();
+    void setName(string n){ name = n; }
+    string getName()const{ return name; }
+    void setNum(int a){ numClasses = a ;}
+    int getNum()const{ return numClasses;}
+    void inputData();
+    void outputData()const;
+    private:
+    string name;
+    int numClasses;
+    string *classList;
+
+};
+Student::Student()
+{
+    name = "";
+    numClasses = 0;
+    classList = nullptr;
+}
+Student::~Student()
+{
+    delete []classList;
+}
+void Student::inputData()
+{
+    
+    classList = new string[numClasses];
+    for(int i=0;i<numClasses;i++)
+    {
+        cout << "Enter your course name: ";
+        cin>> classList[i];
+    }
+}
+void Student::outputData()const
+{
+    cout<< endl;
+    cout << name << " course: " ;
+    for(int i=0;i<numClasses;i++)
+    {
+        cout<< classList[i];
+        if(i!=numClasses-1)
+        {
+            cout <<", ";
+        }
+    }
+}
+int main()
+{
+    Student S;
+    string n;
+    int c;
+    cout << "Enter Student Name: ";
+    cin>> n;
+    S.setName(n);
+    cout << "How many course name enter? ";
+    cin>> c;
+    S.setNum(c);
+    S.inputData();
+    S.outputData();
+}
